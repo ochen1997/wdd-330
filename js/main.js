2e70af51a9dbe58fcb2baf97a36b81d7ad503b1c
ochen1997 <och21003@byui.edu>
2022-04-30 19:11:29 -0600

    week2 notes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const links = [
     {
       label: "Week1 notes",
-      url: "week1/index.html"
-    }
+      url: "week1/index.html",
+      week2label: "week2 notes",
+      week2url: "week2/index.html"
+    }, 
+    
   ]
 
 function loadIndex() {
@@ -10,10 +13,18 @@ function loadIndex() {
     links.forEach(link => {
         const li = document.createElement('li');
         const href = document.createElement('a');
-        href.setAttribute('href', link.url);
+        href.setAttribute('href', link.url)
         href.innerText = link.label
-
         li.appendChild(href);
         ol.appendChild(li);
+        const li2 = document.createElement('li');
+        const href2 = document.createElement('a');
+        href2.setAttribute('href',link.week2url);
+        href2.innerText = link.week2label
+        li2.appendChild(href2);
+        ol.appendChild(li2)
+
+
     })
 }
+
